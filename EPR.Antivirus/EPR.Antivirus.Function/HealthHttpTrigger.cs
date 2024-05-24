cc362f6b7b89a1c80a9c713cdae01afc695a5978
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-05-24 11:02:01 +0100

    [Feature/alerts: Added the HealthHttpTrigger for 500's]

---
+++ EPR.Antivirus/EPR.Antivirus.Function/HealthHttpTrigger.cs
@@ -0,0 +1,22 @@
+﻿namespace EPR.Antivirus.Function
+{
+    using System.Net;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+
+    /// <summary>
+    /// Health check endpoint "api/error".
+    /// "/api/error" will return 500 Internal Server Error.
+    /// </summary>
+    public static class HealthHttpTrigger
+    {
+        [FunctionName(nameof(HealthHttpTrigger))]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "error")] HttpRequest req)
+        {
+            return new OkObjectResult("Error") { StatusCode = (int)HttpStatusCode.InternalServerError };
+        }
+    }
+}
