6b5275ad28849fdfc10bd3b28453838e767ad454
Shehzad Ismail <85490459+shehzad-ismail8@users.noreply.github.com>
2024-08-15 12:32:03 +0100

    [feature/380329-upgrade-to-dotnet8-v2] (#18)
    * [feature/380329-upgrade-to-dotnet8-v2: Upgraded to .NET 8 and all relevant packages to latest]
    
    * [feature/380329-upgrade-to-dotnet8-v2: Updated release candidate]
    
    * upgrade epr-common-logging
    
    ---------
    
    Co-authored-by: Shehzad Ismail <shehzad.ismail@eviden.com>
    Co-authored-by: Ehsan Hatami <ehsan.hatamialamdari.external@eviden.com>

--- EPR.Antivirus/EPR.Antivirus.Application/Exceptions/TradeAntiVirusApiClientException.cs
+++ EPR.Antivirus/EPR.Antivirus.Application/Exceptions/TradeAntiVirusApiClientException.cs
@@ -1,10 +1,8 @@
 ﻿namespace EPR.Antivirus.Application.Exceptions;
 
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.Serialization;
 
 [ExcludeFromCodeCoverage]
-[Serializable]
 public class TradeAntivirusApiClientException : Exception
 {
     public TradeAntivirusApiClientException()
@@ -18,11 +16,6 @@ public class TradeAntivirusApiClientException : Exception
 
     public TradeAntivirusApiClientException(string message, Exception inner)
         : base(message, inner)
-    {
-    }
-
-    protected TradeAntivirusApiClientException(SerializationInfo info, StreamingContext context)
-        : base(info, context)
     {
     }
 }
