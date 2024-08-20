bd59ec18c520378fe7141fe57027bb2b83fcda64
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2024-08-20 07:14:53 +0100

    Remove unnecessary FeatureManagementConfig
    Feature flags are loaded in the nuget package, no need for this config

--- EPR.Antivirus/EPR.Antivirus.Data/Config/FeatureManagementConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace EPR.Antivirus.Data.Config;
-
-using System.Diagnostics.CodeAnalysis;
-
-[ExcludeFromCodeCoverage]
-public class FeatureManagementConfig
-{
-    public const string Section = "FeatureManagement";
-}
